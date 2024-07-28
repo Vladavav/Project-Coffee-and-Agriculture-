2239c349cf6d8ec8fb1f092ed3dfd6240df21a2d
Vladavav <164833013+Vladavav@users.noreply.github.com>
2024-07-28 16:44:01 +0700

    Add files via upload

--- models/preprocess.py
+++ models/preprocess.py
@@ -1,10 +1,15 @@
 from torchvision import transforms as T
+import torch
+import numpy as np 
 
 process = T.Compose([
     T.Resize((224, 224)),
     T.ToTensor()
-    ]
-)
+])
 
 def preprocess(img):
-    return process(img)
+    """Preprocesses an image for use with a PyTorch model."""
+    img = process(img)
+    # Directly convert to a PyTorch tensor
+    img = torch.from_numpy(np.array(img, dtype=np.float32))
+    return img
