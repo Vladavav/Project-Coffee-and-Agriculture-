dda1118dfe3721cc865dc5c7b82be32500c1695a
Vladavav <164833013+Vladavav@users.noreply.github.com>
2024-07-28 16:34:36 +0700

    Add files via upload

--- models/preprocess.py
+++ models/preprocess.py
@@ -2,8 +2,7 @@ from torchvision import transforms as T
 
 process = T.Compose([
     T.Resize((224, 224)),
-    T.ToTensor(),
-    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    T.ToTensor()
     ]
 )
 
